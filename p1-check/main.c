ff9e634511212dfa19fd019bca648dbb5ea090dd
grime3ja <grime3ja@dukes.jmu.edu>
2024-09-18 21:55:04 -0400

    just need to switch endian-ness

--- p1-check/main.c
+++ p1-check/main.c
@@ -21,13 +21,31 @@ int main (int argc, char **argv)
 {
     int opt = 0;
 
+    char *filename = NULL;
+    bool h_flag = false;
+
     while ((opt = getopt(argc, argv, "-hH:")) != -1) {
         switch (opt) {
             case 'h':
                 usage(argv);
+                exit(EXIT_SUCCESS);
             case 'H':
-            
+                h_flag = true;
+                filename = optarg;
         }
+    }
+    // if (optind >= argc) {
+    //     usage(argv);
+    //     exit(EXIT_FAILURE);
+    // }
+    if (h_flag) {
+        FILE *file = fopen(filename, "r");
+        elf_hdr_t hdr;
+        if (!read_header(file, &hdr)) {
+            printf("%s\n", "Failed to read file");
+            exit(EXIT_FAILURE);
+        }
+        dump_header(&hdr);
     }
 
     return EXIT_SUCCESS;
--- p1-check/p1-check.c
+++ p1-check/p1-check.c
@@ -16,7 +16,8 @@ bool read_header (FILE *file, elf_hdr_t *hdr)
         return false;
     }
     int ret = fread(hdr, sizeof(elf_hdr_t), 1, file);
-    return ret == 1 && strtol("ELF\0", NULL, 16) == hdr->magic;
+    // TODO fix hard coding?
+    return ret == 1 && 4607045 == hdr->magic;
 }
 
 /**********************************************************************
@@ -25,5 +26,29 @@ bool read_header (FILE *file, elf_hdr_t *hdr)
 
 void dump_header (elf_hdr_t *hdr)
 {
+    // print header in little endian
+    printf("%02x ", hdr->e_version);
+    printf("%x ", hdr->e_entry);
+    printf("%x ", hdr->e_phdr_start);
+    printf("%x ", hdr->e_num_phdr);
+    printf("%x ", hdr->e_symtab);
+    printf("%x ", hdr->e_strtab);
+    printf("%x ", hdr->magic);
+    printf("\n");
+
+    // print rest of output
+    printf("Mini-ELF version %d\n", hdr->e_version);
+    printf("Entry point 0x%x\n", hdr->e_entry);
+    printf("There are %d program headers, starting at offset %d (0x%x)\n", hdr->e_num_phdr, hdr->e_phdr_start, hdr->e_phdr_start);
+    if (hdr->e_symtab == 0) {
+        printf("There is no symbol table present\n");
+    } else {
+        printf("There is a symbol table starting at offset %d (0x%x)\n", hdr->e_symtab, hdr->e_symtab);
+    }
+    if (hdr->e_strtab == 0) {
+        printf("There is no string table present\n");
+    } else {
+        printf("There is a string table starting at offset %d (0x%x)\n", hdr->e_strtab, hdr->e_strtab);
+    }
 }
 
