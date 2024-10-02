df6322607a5e1df82e4d78197f9b7dc39bbce05c
grime3ja <grime3ja@dukes.jmu.edu>
2024-10-02 00:24:49 -0400

    read and dump program headers

--- p2-load/main.c
+++ p2-load/main.c
@@ -1,7 +1,7 @@
 /*
  * CS 261: Main driver
  *
- * Name: 
+ * Name: Jacob Grimes
  */
 
 #include "p1-check.h"
@@ -25,6 +25,104 @@ void usage (char **argv)
 
 int main (int argc, char **argv)
 {
+    // FILE *fp = fopen ("inputs/simple.o", "r");
+    // byte_t *memory = (byte_t*) calloc (32, sizeof (uint8_t));
+    // elf_phdr_t phdr;
+    // phdr.p_offset = 0x30;
+    // phdr.p_vaddr = 0x10;
+    // phdr.p_size = 0x10;
+    // phdr.p_type = DATA;
+    // phdr.p_flags = 0x6;      // 110
+    // phdr.magic = 0xDEADBEEF;
+    // uint8_t expected[] = { 0, 0, 6, 0, 0xef, 0xbe, 0xad, 0xde,
+    //     0x30, 0xf3, 0xf, 0, 0, 0, 0x20, 0x31 };
+    // size_t i = 0;
+    // bool rc = load_segment (fp, memory, &phdr);
+
+
+    int opt = 0;
+
+    bool h_flag = false;
+    bool s_flag = false;
+    // bool m_flag = false;
+    // bool upper_m_flag = false;
+
+    while ((opt = getopt(argc, argv, "-hHafsmM")) != -1) {
+        switch (opt) {
+            case 'h':
+                usage(argv);
+                exit(EXIT_SUCCESS);
+            case 'H':
+                h_flag = true;
+                break;
+            case 'a':
+                h_flag = true;
+                s_flag = true;
+                // m_flag = true;
+                break;
+            case 'f':
+                h_flag = true;
+                s_flag = true;
+                // upper_m_flag = true;
+                break;
+            case 's':
+                s_flag = true;
+                break;
+            // case 'm':
+                // m_flag = true;
+                break;
+            // case 'M':
+                // upper_m_flag = true;
+                break;
+        }
+    }
+    if (argc > optind) {
+        // no file given
+        usage(argv);
+        exit(EXIT_FAILURE);
+    }
+
+    char *filename = argv[optind - 1];
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL) {
+        usage(argv);
+        exit(EXIT_FAILURE);
+    }
+
+    elf_hdr_t hdr;
+    if (!read_header(file, &hdr)) {
+        printf("Failed to read file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    elf_phdr_t *phdr = (elf_phdr_t *) calloc(hdr.e_num_phdr, sizeof(elf_phdr_t));
+    int offset = hdr.e_phdr_start;
+    for (int i = 0; i < hdr.e_num_phdr; i++) {
+        if (!read_phdr(file, offset, &phdr[i])) {
+            printf("Failed to read file\n");
+            exit(EXIT_FAILURE);
+        }
+        offset += 20;
+    }
+    // if (!load_segment(file, mem, &phdr)) {
+    //     printf("Failed to read file\n");
+    //     exit(EXIT_FAILURE);
+    // }
+
+    if (h_flag) {
+        dump_header(&hdr);
+    }
+    if (s_flag) {
+        dump_phdrs(hdr.e_num_phdr, phdr);
+    }
+    // segments
+
+    // contents of memory
+
+    free(phdr);
+    // free(mem);
+    
     return EXIT_SUCCESS;
 }
 
--- p2-load/p2-load.c
+++ p2-load/p2-load.c
@@ -1,7 +1,7 @@
 /*
  * CS 261 PA2: Mini-ELF loader
  *
- * Name: 
+ * Name: Jacob Grimes
  */
 
 #include "p2-load.h"
@@ -12,12 +12,21 @@
 
 bool read_phdr (FILE *file, uint16_t offset, elf_phdr_t *phdr)
 {
-    return true;
+    if (file == NULL || phdr == NULL) {
+        return false;
+    }
+    fseek(file, offset, SEEK_SET);
+    return fread(phdr, sizeof(elf_phdr_t), 1, file) == 1 && phdr->magic == 0xdeadbeef;
 }
 
 bool load_segment (FILE *file, byte_t *memory, elf_phdr_t *phdr)
 {
-    return true;
+    if (file == NULL || memory == NULL || phdr == NULL || phdr->p_vaddr > sizeof(memory)) {
+        return false;
+    }
+    fseek(file, phdr->p_offset, SEEK_SET);
+    int ret = fread(&memory[phdr->p_vaddr], phdr->p_size, 1, file);
+    return ret == 1 && (phdr->p_type == DATA || phdr->p_type == CODE);
 }
 
 /**********************************************************************
@@ -26,6 +35,25 @@ bool load_segment (FILE *file, byte_t *memory, elf_phdr_t *phdr)
 
 void dump_phdrs (uint16_t numphdrs, elf_phdr_t *phdrs)
 {
+    // -s flag
+    printf(" Segment   Offset    Size      VirtAddr  Type      Flags\n");
+    for (int i = 0; i < numphdrs; i++) {
+        printf("  %02x       ", i);
+        printf("0x%04x    ", phdrs[i].p_offset);
+        printf("0x%04x    ", phdrs[i].p_size);
+        printf("0x%04x    ", phdrs[i].p_vaddr);
+        // if type is CODE else DATA
+        printf("%s      ", (DATA + phdrs[i].p_type) == 0 ? "DATA" : "CODE");
+        // if flag is 0x4 (R  ), 0x5 (R X), 0x6 (RW )
+        if (phdrs[i].p_flags == 0x4) {
+            printf("R  ");
+        } else if (phdrs[i].p_flags == 0x5) {
+            printf("R X");
+        } else {
+            printf("RW ");
+        }
+        printf("\n");
+    }
 }
 
 void dump_memory (byte_t *memory, uint16_t start, uint16_t end)
