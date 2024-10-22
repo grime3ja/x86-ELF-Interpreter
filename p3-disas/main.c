79bada4dc9b7a42c687801ecd44e9068abb68387
grime3ja <grime3ja@dukes.jmu.edu>
2024-10-21 23:19:44 -0400

    solid start

--- p3-disas/main.c
+++ p3-disas/main.c
@@ -28,6 +28,119 @@ void usage (char **argv)
 
 int main (int argc, char **argv)
 {
+    int opt;
+
+    bool h_flag = false;
+    bool s_flag = false;
+    bool m_flag = false;
+    bool upper_m_flag = false;
+    bool d_flag = false;
+    bool upper_d_flag = false;
+
+    while ((opt = getopt(argc, argv, "-hHafsmMdD")) != 1) {
+        switch (opt) {
+            case 'h':
+                usage(argv);
+                exit(EXIT_SUCCESS);
+            case 'H':
+                h_flag = true;
+                break;
+            case 'a':
+                h_flag = true;
+                s_flag = true;
+                m_flag = true;
+                break;
+            case 'f':
+                h_flag = true;
+                s_flag = true;
+                upper_m_flag = true;
+                break;
+            case 's':
+                s_flag = true;
+                break;
+            case 'm':
+                m_flag = true;
+                break;
+            case 'M':
+                upper_m_flag = true;
+                break;
+            case 'd':
+                d_flag = true;
+            case 'D':
+                upper_d_flag = true;
+        }
+    }
+
+    if (argc > optind) {
+        // no file given
+        usage(argv);
+        exit(EXIT_FAILURE);
+    }
+
+    char *filename = argv[optind - 1];
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL) {
+        usage(argv);
+        exit(EXIT_FAILURE);
+    }
+
+    elf_hdr_t hdr;
+    if (!read_header(file, &hdr)) {
+        printf("Failed to read file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    elf_phdr_t *phdr = 
+        (elf_phdr_t *) calloc(hdr.e_num_phdr, sizeof(elf_phdr_t));
+    byte_t *mem = (byte_t *) calloc(MEMSIZE, 1);
+    int offset = hdr.e_phdr_start;
+    for (int i = 0; i < hdr.e_num_phdr; i++) {
+        if (!read_phdr(file, offset, &phdr[i])) {
+            printf("Failed to read file\n");
+            exit(EXIT_FAILURE);
+        }
+        offset += 20;
+    }
+    for (int i = 0; i < hdr.e_num_phdr; i++) {
+        if (!load_segment(file, mem, &phdr[i])) {
+            printf("Failed to read file\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    // y86_t cpu;
+    // y86_inst_t inst;
+
+    // for (int i = 0; i < phdr->p_size; i++) {
+    //     inst = fetch(&cpu, &mem[cpu.pc]);
+    //     cpu.pc += inst.valP;
+    // }
+
+    if (m_flag && upper_m_flag) {
+        usage(argv);
+        exit(EXIT_FAILURE);
+    }
+    if (h_flag) {
+        dump_header(&hdr);
+    }
+    if (s_flag) {
+        dump_phdrs(hdr.e_num_phdr, phdr);
+    }
+    if (m_flag) {
+        for (int i = 0; i < hdr.e_num_phdr; i++) {
+            dump_memory
+                (mem, phdr[i].p_vaddr, phdr[i].p_vaddr + phdr[i].p_size);
+        }
+    }
+    if (upper_m_flag) {
+        dump_memory(mem, 0, MEMSIZE);
+    }
+    if (d_flag) {
+        disassemble_code(mem, phdr, &hdr);
+    }
+
+    free(phdr);
+    free(mem);
     return EXIT_SUCCESS;
 }
 
--- p3-disas/p3-disas.c
+++ p3-disas/p3-disas.c
@@ -13,7 +13,41 @@
 y86_inst_t fetch (y86_t *cpu, byte_t *memory)
 {
     y86_inst_t ins;
-
+    if (cpu == NULL) {
+        ins.icode = INVALID;
+        return ins;
+    }
+    if (memory == NULL) {
+        ins.icode = INVALID;
+        cpu->stat = INS;
+        return ins;
+    }
+    switch (memory[cpu->pc] >> 4) {
+        case HALT:
+            ins.icode = HALT;
+            ins.ifun.b = 0;
+            ins.valP = cpu->pc + 1;
+            break;
+        case NOP:
+            ins.icode = NOP;
+            ins.ifun.b = 0;
+            ins.valP = cpu->pc + 1;
+            break;
+        case RET:
+            ins.icode = RET;
+            ins.ifun.b = 0;
+            ins.valP = cpu->pc + 1;
+            break;
+        // case CMOV:
+        //     ins.icode = CMOV;
+        //     ins.ifun.cmov = CMOV;
+        //     ins.rb = memory[cpu->pc + 1];
+        //     ins.ra = memory[(cpu->pc + 1) >> 8];
+        // default:
+        //     ins.icode = INVALID;
+        //     cpu->stat = INS;
+        //     break;
+    }
     return ins;
 }
 
@@ -23,10 +57,47 @@ y86_inst_t fetch (y86_t *cpu, byte_t *memory)
 
 void disassemble (y86_inst_t *inst)
 {
+    switch(inst->icode) {
+        case HALT:
+            printf("halt\n");
+            break;
+        case NOP:
+            printf("nop\n");
+            break;
+        case RET:
+            printf("ret\n");
+            break;
+    }
 }
 
 void disassemble_code (byte_t *memory, elf_phdr_t *phdr, elf_hdr_t *hdr)
 {
+    y86_t cpu;			// CPU struct to store "fake" PC
+    y86_inst_t ins;		// struct to hold fetched instruction
+
+    // start at beginning of the segment
+    cpu.pc = phdr->p_vaddr;
+
+    printf("Disassembly of executable contents:\n");
+    printf("  0x100:                               | .pos 0x100 code\n");
+    printf("  0x100:                               | _start:\n");
+
+    // iterate through the segment one instruction at a time
+    while (cpu.pc < phdr->p_vaddr + phdr->p_size) {
+        ins = fetch (&cpu, memory);         // stage 1: fetch instruction
+
+        // abort with error if instruction was invalid
+        if (ins.icode == INVALID) {
+            printf("Invalid opcode: 0x%2x\n", memory[cpu.pc]);
+            break;
+        }
+        // TODO: print current address and raw bytes of instruction
+        printf("  0x%lx: %02x", cpu.pc, ins.icode << 4);
+
+        disassemble (&ins);                 // stage 2: print disassembly
+        cpu.pc = ins.valP;                  // stage 3: update PC (go to next instruction)
+    }
+    printf("\n");
 }
 
 void disassemble_data (byte_t *memory, elf_phdr_t *phdr)
