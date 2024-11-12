34063093edf75ccc5ab5d37ca0c37ddbb94126e1
grime3ja <grime3ja@dukes.jmu.edu>
2024-11-12 18:14:14 -0500

    D tests

--- p4-interp/main.c
+++ p4-interp/main.c
@@ -1,7 +1,7 @@
 /*
  * CS 261: Main driver
  *
- * Name: 
+ * Name: Jacob Grimes
  */
 
 #include "p1-check.h"
@@ -31,6 +31,186 @@ void usage (char **argv)
 
 int main (int argc, char **argv)
 {
+    int opt;
+
+    bool h_flag = false;
+    bool s_flag = false;
+    bool m_flag = false;
+    bool upper_m_flag = false;
+    bool d_flag = false;
+    bool upper_d_flag = false;
+    bool e_flag = false;
+    bool upper_e_flag = false;
+
+
+    while ((opt = getopt(argc, argv, "hHafsmMdDeE")) != -1) {
+        switch (opt) {
+            case 'h':
+                usage(argv);
+                exit(EXIT_SUCCESS);
+            case 'H':
+                h_flag = true;
+                break;
+            case 'a':
+                h_flag = true;
+                s_flag = true;
+                m_flag = true;
+                break;
+            case 'f':
+                h_flag = true;
+                s_flag = true;
+                upper_m_flag = true;
+                break;
+            case 's':
+                s_flag = true;
+                break;
+            case 'm':
+                m_flag = true;
+                break;
+            case 'M':
+                upper_m_flag = true;
+                break;
+            case 'd':
+                d_flag = true;
+                break;
+            case 'D':
+                upper_d_flag = true;
+                break;
+            case 'e':
+                e_flag = true;
+                break;
+            case 'E':
+                upper_e_flag = true;
+                break;
+            default:
+                usage(argv);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (argv[optind + 1] != NULL) {
+        usage(argv);
+        exit(EXIT_FAILURE);
+    }
+
+    char *filename = argv[optind];
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL) {
+        printf("Failed to read file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    elf_hdr_t hdr;
+    if (!read_header(file, &hdr)) {
+        printf("Failed to read file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    elf_phdr_t *phdr =
+        (elf_phdr_t *) calloc(hdr.e_num_phdr, sizeof(elf_phdr_t));
+    byte_t *mem = (byte_t *) calloc(MEMSIZE, 1);
+    int offset = hdr.e_phdr_start;
+    for (int i = 0; i < hdr.e_num_phdr; i++) {
+        if (!read_phdr(file, offset, &phdr[i])) {
+            printf("Failed to read file\n");
+            exit(EXIT_FAILURE);
+        }
+        offset += 20;
+    }
+    for (int i = 0; i < hdr.e_num_phdr; i++) {
+        if (!load_segment(file, mem, &phdr[i])) {
+            printf("Failed to read file\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (m_flag && upper_m_flag) {
+        usage(argv);
+        exit(EXIT_FAILURE);
+    }
+    if (h_flag) {
+        dump_header(&hdr);
+    }
+    if (s_flag) {
+        dump_phdrs(hdr.e_num_phdr, phdr);
+    }
+    if (m_flag) {
+        for (int i = 0; i < hdr.e_num_phdr; i++) {
+            dump_memory
+            (mem, phdr[i].p_vaddr, phdr[i].p_vaddr + phdr[i].p_size);
+        }
+    }
+    if (upper_m_flag) {
+        dump_memory(mem, 0, MEMSIZE);
+    }
+
+    y86_t cpu;
+    y86_inst_t inst;
+    bool cnd;
+    y86_reg_t valA, valE;
+
+    memset(&cpu.reg, 0, sizeof(cpu.reg));
+
+    cpu.stat = AOK;
+    cpu.pc = hdr.e_entry;
+
+    if (upper_e_flag || e_flag) {
+        printf("Beginning execution at 0x%04x\n", hdr.e_entry);
+        if (upper_e_flag) {
+            dump_cpu_state(&cpu);
+        }
+    }
+
+    while (cpu.stat == AOK) {
+        // p3
+        inst = fetch(&cpu, mem);
+
+        if (d_flag) {
+            printf("Disassembly of executable contents:\n");
+            for (int i = 0; i < hdr.e_num_phdr; i++) {
+                if (phdr[i].p_type == CODE) {
+                    disassemble_code(mem, &phdr[i], &hdr);
+                }
+            }
+        }
+
+        if (upper_d_flag) {
+            printf("Disassembly of data contents:\n");
+            for (int i = 0; i < hdr.e_num_phdr; i++) {
+                if (phdr[i].p_type == DATA && phdr[i].p_flags > 4) {
+                    disassemble_data(mem, &phdr[i]);
+                } else if (phdr[i].p_type == DATA && phdr[i].p_flags == 4) {
+                    disassemble_rodata(mem, &phdr[i]);
+                }
+            }
+        }
+
+        // p4
+
+        valE = decode_execute(&cpu, &inst, &cnd, &valA);
+        memory_wb_pc(&cpu, &inst, mem, cnd, valA, valE);
+
+        if (upper_e_flag) {
+            printf("\nExecuting: ");
+            disassemble(&inst);
+            printf("\n");
+            dump_cpu_state(&cpu);
+        }
+    }
+    if (e_flag) {
+        dump_cpu_state(&cpu);
+    }
+    if (upper_e_flag || e_flag) {
+        printf("Total execution count: %d\n", hdr.e_num_phdr);
+    }
+    if (upper_e_flag) {
+        printf("\n");
+        dump_memory(mem, 0, MEMSIZE);
+    }
+
+    free(phdr);
+    free(mem);
     return EXIT_SUCCESS;
 }
 
--- p4-interp/p4-interp.c
+++ p4-interp/p4-interp.c
@@ -1,7 +1,7 @@
 /*
  * CS 261 PA4: Mini-ELF interpreter
  *
- * Name: 
+ * Name: Jacob Grimes
  */
 
 #include "p4-interp.h"
@@ -13,13 +13,67 @@
 y86_reg_t decode_execute (y86_t *cpu, y86_inst_t *inst, bool *cnd, y86_reg_t *valA)
 {
     y86_reg_t valE = 0;
-
+    if (cpu == NULL) {
+        inst->icode = INVALID;
+        return valE;
+    }
+    if (inst == NULL || cnd == NULL || valA == NULL) {
+        cpu->stat = INS;
+        inst->icode = INVALID;
+        return valE;
+    }
+    switch (inst->icode) {
+        case HALT:
+            cpu->stat = HLT;
+            break;
+        case NOP:
+            break;
+        case CMOV:
+            // *valA = inst->ra;
+            // valA = cpu->reg[]
+            valE = *valA;
+            // Cnd ← Cond(CC,ifun)
+        case IRMOVQ:
+            valE = inst->valC.v;
+            break;
+        case RMMOVQ:
+            *valA = inst->ra;
+            valE = inst->rb + inst->valC.d;
+            break;
+        case MRMOVQ:
+            valE = inst->rb + inst->valC.d;
+            break;
+        default:
+            break;
+    }
     return valE;
 }
 
 void memory_wb_pc (y86_t *cpu, y86_inst_t *inst, byte_t *memory,
         bool cnd, y86_reg_t valA, y86_reg_t valE)
 {
+    switch (inst->icode) {
+        case HALT:
+            cpu->pc = inst->valP;
+            break;
+        case NOP:
+            cpu->pc = inst->valP;
+            break;
+        case CMOV:
+            // Cnd ? R[rB] ← valE 
+            break;
+        case IRMOVQ:
+            inst->rb = valE;
+            break;
+        case RMMOVQ:
+            memcpy(&valE, &valA, 8);
+            break;
+        case MRMOVQ:
+            memcpy(&(inst->ra), &valE, 8);
+            break;
+        default:
+            break;
+    }
 }
 
 /**********************************************************************
@@ -28,5 +82,33 @@ void memory_wb_pc (y86_t *cpu, y86_inst_t *inst, byte_t *memory,
 
 void dump_cpu_state (y86_t *cpu)
 {
+    printf("Y86 CPU state:\n");
+
+    printf("    PC: %016lx   ", cpu->pc);
+    printf("flags: Z%d S%d O%d     ", cpu->zf & 0xf, cpu->sf & 0xf, cpu->of & 0xf);
+    switch (cpu->stat) {
+        case AOK:
+            printf("AOK");
+            break;
+        case HLT:
+            printf("HLT");
+            break;
+        case ADR:
+            printf("ADR");
+            break;
+        default:
+            printf("INS");
+            break;
+    }
+    printf("\n");
+    
+    printf("  %%rax: %016lx    %%rcx: %016lx\n", cpu->reg[0], cpu->reg[1]);
+    printf("  %%rdx: %016lx    %%rbx: %016lx\n", cpu->reg[2], cpu->reg[3]);
+    printf("  %%rsp: %016lx    %%rbp: %016lx\n", cpu->reg[4], cpu->reg[5]);
+    printf("  %%rsi: %016lx    %%rdi: %016lx\n", cpu->reg[6], cpu->reg[7]);
+    printf("   %%r8: %016lx     %%r9: %016lx\n", cpu->reg[8], cpu->reg[9]);
+    printf("  %%r10: %016lx    %%r11: %016lx\n", cpu->reg[10], cpu->reg[11]);
+    printf("  %%r12: %016lx    %%r13: %016lx\n", cpu->reg[12], cpu->reg[13]);
+    printf("  %%r14: %016lx\n", cpu->reg[14]);
 }
 
